ff8657b9d6a1d06c8b1ed00a2ae7706ecaa2a730
Hajin Kim <rhrnak624@naver.com>
2022-03-23 14:11:07 +0900

    Java를 이용한 Constructor DI, Setter DI 실습

--- JavaDI/src/main/java/kr/hs/study/main/MainClass.java
+++ JavaDI/src/main/java/kr/hs/study/main/MainClass.java
@@ -1,10 +1,42 @@
 package kr.hs.study.main;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import kr.hs.study.beans.TestBean3;
+import kr.hs.study.beans.TestBean4;
+import kr.hs.study.config.BeanConfigClass;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(BeanConfigClass.class);
+		
+		TestBean3 obj1 = ctx.getBean("test", TestBean3.class);
+		System.out.println("obj1 : " + obj1);
+		
+		System.out.println("===========================");
+		
+		TestBean3 obj2 = ctx.getBean("test", TestBean3.class);
+		System.out.println("obj2 : " + obj2);
+		
+		System.out.println("===========================");
+		
+		TestBean4 obj3 = ctx.getBean("java4", TestBean4.class);
+		System.out.println(obj3.getData1());
+		System.out.println(obj3.getData2());
+		System.out.println(obj3.getData4());
+		
+		System.out.println("===========================");
+		
+		TestBean4 obj4 = ctx.getBean("java5", TestBean4.class);
+		System.out.println(obj4.getData1());
+		System.out.println(obj4.getData2());
+		System.out.println(obj4.getData4());
+		
+		
+		ctx.close();
+	
 	}
 
 }
